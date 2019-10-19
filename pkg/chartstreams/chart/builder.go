383ab89b24a197730f33b6a7a63a6420ab33e934
Igor Sutton <igorsutton@me.com>
2019-10-19 09:10:46 +0200

    Add docs to chart.Builder interface

--- pkg/chartstreams/chart/builder.go
+++ pkg/chartstreams/chart/builder.go
@@ -4,8 +4,12 @@ import "time"
 
 // Builder provides a fluent interface to build charts.
 type Builder interface {
+	// Build produces a chart Package.
 	Build() (*Package, error)
+	// SetChartName configures the name of the chart.
 	SetChartName(string) Builder
+	// SetChartPath configures the path of the chart.
 	SetChartPath(string) Builder
+	// SetCommitTime configures the chart's creation time.
 	SetCommitTime(time.Time) Builder
 }

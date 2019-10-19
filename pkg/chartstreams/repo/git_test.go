51578df461f867798b38353472d2b38ad45c63a2
Igor Sutton <igorsutton@me.com>
2019-10-19 13:22:20 +0200

    Refactor tests as a test table

--- pkg/chartstreams/repo/git_test.go
+++ pkg/chartstreams/repo/git_test.go
@@ -6,18 +6,27 @@ import (
 	"github.com/otaviof/chart-streams/pkg/chartstreams/config"
 )
 
-func TestNewGitChartRepository_ExistingRemoteRepository_ShouldSucceed(t *testing.T) {
-	cfg := &config.Config{Depth: 1, RepoURL: "https://github.com/helm/charts.git"}
-	_, err := NewGitChartRepository(cfg)
-	if err != nil {
-		t.Error(err)
+func TestNewGitChartRepository(t *testing.T) {
+	tests := []struct {
+		name       string
+		repoURL    string
+		shouldFail bool
+	}{
+		{name: "existing remote repository", repoURL: "https://github.com/helm/charts.git", shouldFail: false},
+		{name: "non-existing remote repository", repoURL: "https://example.com/charts.git", shouldFail: true},
 	}
-}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			cfg := &config.Config{Depth: 1, RepoURL: test.repoURL}
+			_, err := NewGitChartRepository(cfg)
 
-func TestNewGitChartRepository_UnknownRemoteRepository_ShouldFail(t *testing.T) {
-	cfg := &config.Config{Depth: 1, RepoURL: "https://example.com/charts.git"}
-	_, err := NewGitChartRepository(cfg)
-	if err == nil {
-		t.Error("unknown remote repository should fail, but it isn't")
+			if test.shouldFail && err == nil {
+				t.Fail()
+			}
+
+			if !test.shouldFail && err != nil {
+				t.Fail()
+			}
+		})
 	}
 }

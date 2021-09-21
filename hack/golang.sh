7890947b3333b23815b6a632a2322d05e9b63ecd
Igor Sutton <igor@moreausutton.com>
2021-09-21 14:31:43 +0000

    Recommend Go extension in DevContainer

--- hack/golang.sh
+++ hack/golang.sh
@@ -11,6 +11,3 @@ yum install \
     --allowerasing \
     --enablerepo=rawhide \
     golang
-
-GOLANGCI_LINT_VERSION=${GOLANGCI_LINT_VERSION:-1.42.1}
-rpm -i https://github.com/golangci/golangci-lint/releases/download/v${GOLANGCI_LINT_VERSION}/golangci-lint-${GOLANGCI_LINT_VERSION}-linux-amd64.rpm

2af8bfe98015b34ce82c15e2cfe9e1742c8a72b5
Igor Sutton <igorsutton@me.com>
2021-09-24 18:12:18 +0000

    Use /build as GOTMPDIR in devcontainer-run.sh

--- hack/devcontainer-run.sh
+++ hack/devcontainer-run.sh
@@ -3,8 +3,8 @@
 docker run \
     --rm \
     --interactive \
+    --tty \
     --env GOTMPDIR=/build \
-    --env CODECOV_TOKEN="${CODECOV_TOKEN}" \
     --volume="${PWD}:/src/${APP}" \
     --workdir="/src/${APP}" \
     ${IMAGE_DEV_TAG} ${DEVCONTAINER_ARGS}

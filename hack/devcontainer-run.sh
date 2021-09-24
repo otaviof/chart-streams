2ef15b68d9feb21f8732d845cd1c94ec05edfab2
Igor Sutton <igorsutton@me.com>
2021-09-24 18:12:18 +0000

    Use /build as GOTMPDIR in devcontainer-run.sh

--- hack/devcontainer-run.sh
+++ hack/devcontainer-run.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-TMPDIR=$(mktemp -d)
-
 docker run \
     --rm \
     --interactive \
     --tty \
-    --mount type=bind,source="${TMPDIR}",destination=/tmp \
+    --env GOTMPDIR=/build \
     --volume="${PWD}:/src/${APP}" \
     --workdir="/src/${APP}" \
     ${IMAGE_DEV_TAG} ${DEVCONTAINER_ARGS}
 
-rm -fr "${TMPDIR}"

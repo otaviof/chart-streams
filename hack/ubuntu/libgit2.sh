df480953b62527d00279c148ab8877b7b28d6761
Igor Sutton <isuttonl@redhat.com>
2021-09-28 11:37:55 +0000

    Build on action host

---
+++ hack/ubuntu/libgit2.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#
+# Installs libgit2 from source-code, after downloading and preparing the system to compile it.
+#
+
+set -eu
+
+LIBGIT_HOST="github.com"
+LIBGIT_PATH="libgit2/libgit2/archive/refs/tags"
+
+LIBGIT_VERSION=${LIBGIT_VERSION:-1.2.0}
+
+LIBGIT_TARBALL="v${LIBGIT_VERSION}.tar.gz"
+LIBGIT_DIR="libgit2-${LIBGIT_VERSION}"
+LIBGIT_PREFIX="/usr/local"
+
+TMPDIR="$(mktemp -d)"
+cd ${TMPDIR}
+
+echo "# Installing dependencies..."
+apt install --yes --quiet \
+    curl \
+    cmake \
+    libssl-dev \
+    python
+
+echo "# Downloading libgit2 source-code..."
+curl --silent --location --remote-name https://${LIBGIT_HOST}/${LIBGIT_PATH}/${LIBGIT_TARBALL}
+
+echo "# Extracting tarball..."
+tar xvpf ${LIBGIT_TARBALL}
+cd ${LIBGIT_DIR}
+
+echo "# Building and installing libgit2!"
+mkdir build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX="${LIBGIT_PREFIX}"
+cmake --build . --target install
+
+echo "# Remmoving temporary director '${TMPDIR}'"
+cd /tmp && rm -rf "${TMPDIR}"
+
+echo "# Installed:"
+ls -l "${LIBGIT_PREFIX}/lib/libgit2.so.${LIBGIT_VERSION}"

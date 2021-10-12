otaviof/chart-streams

commit 2c8c8cedeb92ad5d42df5a90222561ab0e0f2d33

Build index.yaml from the Git index instead of a checkout (#21)
* Clone the bare repository

* Build index.yaml directly from the Git index

pkg/chartstreams/index_builder.go
@@ -2,9 +2,7 @@ package chartstreams
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
-	"path"
 	"strings"
 
 	"helm.sh/helm/v3/pkg/chart/loader"
@@ -42,31 +40,26 @@ func (i *IndexBuilder) GetChartCommitInfo(name, version string) *CommitInfo {
 	return nil
 }
 
-// basePath returns the base path to charts repository.
-func (i *IndexBuilder) basePath() string {
-	return path.Join(i.g.WorkingDir, i.cfg.RelativeDir)
-}
-
-// ChartAbsPath returns the absolute path to informed chart.
-func (i *IndexBuilder) ChartAbsPath(name string) string {
-	return path.Join(i.basePath(), name)
-}
-
 // listAllChartDirs returns a list of all charts found on base chart directory.
-func (i *IndexBuilder) listAllChartDirs() ([]string, error) {
-	dirs, err := ioutil.ReadDir(i.basePath())
+func (i *IndexBuilder) listAllChartDirs(c *git.Commit) ([]string, error) {
+	tree, err := c.Tree()
 	if err != nil {
-		return nil, fmt.Errorf("reading base path %q: %w", i.basePath(), err)
+		return nil, fmt.Errorf("getting commit's tree: %w", err)
 	}
+	defer tree.Free()
 
 	var chartDirs []string
-	for _, dir := range dirs {
-		if !dir.IsDir() || strings.HasPrefix(dir.Name(), ".") {
-			log.Tracef("Skipping entry in directory: '%s'", dir.Name())
-			continue
+	tree.Walk(func(path string, te *git.TreeEntry) int {
+		if te.Filemode != git.FilemodeTree {
+			return 0
 		}
-		chartDirs = append(chartDirs, dir.Name())
-	}
+		parts := strings.Split(path, "/")
+		if len(parts) > 1 || len(parts) == 0 {
+			return 0
+		}
+		chartDirs = append(chartDirs, parts[0])
+		return 0
+	})
 	return chartDirs, nil
 }
 
@@ -81,8 +74,8 @@ func (i *IndexBuilder) relativeDir() string {
 
 // listModifiedChartDirs inspect git commit to discover which charts have changed, return the list
 // of modified charts.
-func (i *IndexBuilder) listModifiedChartDirs(c *git.Commit, tree *git.Tree) ([]string, error) {
-	modified, err := i.g.ModifiedFiles(c, tree)
+func (i *IndexBuilder) listModifiedChartDirs(c *git.Commit) ([]string, error) {
+	modified, err := i.g.ModifiedFiles(c)
 	if err != nil {
 		return nil, fmt.Errorf("obtaining modified files for commit-id %q: %w", c.Id(), err)
 	}
@@ -173,9 +166,11 @@ func (i *IndexBuilder) register(
 // inspectDirs loop chart directories, loading charts and registering them.
 func (i *IndexBuilder) inspectDirs(dirs []string, revision string, c *git.Commit, head bool) error {
 	for _, dir := range dirs {
-		chartAbsPath := i.ChartAbsPath(dir)
-		log.Debugf("Loading chart from '%s'...", chartAbsPath)
-		chart, err := loader.LoadDir(chartAbsPath)
+		files, err := i.g.GetFilesFromCommit(c, dir)
+		if err != nil {
+			return fmt.Errorf("getting files from commit '%s': %w", c.Id(), err)
+		}
+		chart, err := loader.LoadFiles(files)
 		if err != nil {
 			log.Warnf("error loading chart: '%s'", err)
 			continue
@@ -204,15 +199,15 @@ func (i *IndexBuilder) inspectDirs(dirs []string, revision string, c *git.Commit
 // walk through git commits in order to identify which charts have changed, or to load all charts
 // from repository in case of master's HEAD.
 func (i *IndexBuilder) walk() error {
-	return i.g.CommitIter(func(branch string, c *git.Commit, tree *git.Tree, head bool) error {
+	return i.g.CommitIter(func(branch string, c *git.Commit, head bool) error {
 		log.Infof("Inspecting commit-id '%s/%s'", branch, c.Id().String())
 		var chartDirs []string
 		var err error
 		if head && branch == "master" {
 			log.Infof("HEAD: Retrieving all chart directories...")
-			chartDirs, err = i.listAllChartDirs()
+			chartDirs, err = i.listAllChartDirs(c)
 		} else {
-			chartDirs, err = i.listModifiedChartDirs(c, tree)
+			chartDirs, err = i.listModifiedChartDirs(c)
 		}
 		if err != nil {
 			return err

pkg/chartstreams/chart_provider_test.go
@@ -15,23 +15,6 @@ func init() {
 	SetLogLevel("trace")
 }
 
-func requirePopulatedGitDir(t *testing.T, dir string) {
-	files, err := ioutil.ReadDir(dir)
-	require.NoError(t, err)
-
-	count := 0
-	for _, file := range files {
-		if file.Name() == ".git" {
-			t.Logf("Skipping '%s' on '%s'", file.Name(), dir)
-			continue
-		}
-		if file.IsDir() {
-			count++
-		}
-	}
-	require.Truef(t, count > 0, "expected to find directories on '%s' path", dir)
-}
-
 func TestGitChartProvider(t *testing.T) {
 	helmChartName := "one"
 	helmRepoDir, err := util.ChartsRepoDir("../..")
@@ -51,7 +34,6 @@ func TestGitChartProvider(t *testing.T) {
 	t.Run("Initialize", func(t *testing.T) {
 		err := g.Initialize()
 		require.NoError(t, err)
-		requirePopulatedGitDir(t, tempDir)
 	})
 
 	var helmChartVersion string

pkg/chartstreams/git_repo.go
@@ -29,16 +29,13 @@ type GitRepo struct {
 }
 
 // CommitIterFn function to be executed against each commit.
-type CommitIterFn func(string, *git.Commit, *git.Tree, bool) error
+type CommitIterFn func(string, *git.Commit, bool) error
 
 // branchIterFn function to be executed against each branch.
-type branchIterFn func(string, *git.Odb) error
+type branchIterFn func(string, *git.Commit) error
 
 // originPrefix common origin string prefix.
 const originPrefix = "origin/"
-
-// checkoutOpts common checkout options, to force and keep a clean tree.
-var checkoutOpts = &git.CheckoutOptions{Strategy: git.CheckoutForce | git.CheckoutRemoveUntracked}
 
 // sortBranches will sort local list of branches, skipping "master".
 func (g *GitRepo) sortBranches() []string {
@@ -52,8 +49,8 @@ func (g *GitRepo) sortBranches() []string {
 	return sorted
 }
 
-// lookupBranch search for a remote branch, and if not found, a local branch instead.
-func (g *GitRepo) lookupBranch(branch string) (*git.Branch, error) {
+// LookupBranch search for a remote branch, and if not found, a local branch instead.
+func (g *GitRepo) LookupBranch(branch string) (*git.Branch, error) {
 	remoteBranch := fmt.Sprintf("%s%s", originPrefix, branch)
 	b, err := g.r.LookupBranch(remoteBranch, git.BranchRemote)
 	if err == nil && b != nil {
@@ -63,18 +60,6 @@ func (g *GitRepo) lookupBranch(branch string) (*git.Branch, error) {
 
 	log.Infof("Searching for local branch '%s'...", branch)
 	return g.r.LookupBranch(branch, git.BranchLocal)
-}
-
-// checkoutTree execute tree look up and checkout.
-func (g *GitRepo) checkoutTree(oid *git.Oid) (*git.Tree, error) {
-	tree, err := g.r.LookupTree(oid)
-	if err != nil {
-		return nil, fmt.Errorf("looking up tree %q: %w", oid, err)
-	}
-	if err = g.r.CheckoutTree(tree, checkoutOpts); err != nil {
-		return nil, fmt.Errorf("checking out tree %q: %w", oid, err)
-	}
-	return tree, nil
 }
 
 // GetFilesFromCommit returns a list of files inside the path for a given
@@ -88,6 +73,7 @@ func (g *GitRepo) GetFilesFromCommit(
 	if err != nil {
 		return nil, fmt.Errorf("obtaining commit tree: %w", err)
 	}
+	defer tree.Free()
 
 	// files contains the contents to be returned, ready to be used by
 	// `loader.LoadFiles`.
@@ -128,79 +114,51 @@ func (g *GitRepo) GetFilesFromCommit(
 	return files, err
 }
 
-// CheckoutCommit based in branch and commit id, execute tree checkout.
-func (g *GitRepo) CheckoutCommit(branch string, c *git.Commit) error {
-	log.Infof("Checking out commit-id '%s/%s'", branch, c.Id().String())
-	tree, err := g.checkoutTree(c.TreeId())
-	if err != nil {
-		return err
-	}
-	defer tree.Free()
-
-	r, err := g.r.References.Create(fmt.Sprintf("refs/head/%s", branch), c.Id(), true, branch)
-	if err != nil {
-		return err
-	}
-	defer r.Free()
-
-	head := fmt.Sprintf("refs/heads/%s", branch)
-	log.Debugf("Setting head as '%s' for branch '%s'", head, branch)
-	err = g.r.SetHead(head)
-	if err != nil {
-		return err
-	}
-	return g.r.CheckoutHead(checkoutOpts)
-}
-
-// checkoutBranch look up branch, and look up head commit, with this information it can checkout the
+// LookupBranchCommit look up branch, and look up head commit, with this information it can checkout the
 // branch tree and finally, set repository information about new head.
-func (g *GitRepo) checkoutBranch(branch string) error {
-	log.Infof("Checking out branch '%s' HEAD...", branch)
-	b, err := g.lookupBranch(branch)
+func (g *GitRepo) LookupBranchCommit(branch string) (*git.Commit, error) {
+	b, err := g.LookupBranch(branch)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer b.Free()
 
 	c, err := g.r.LookupCommit(b.Target())
 	if err != nil {
-		return err
+		return nil, err
 	}
-	defer c.Free()
-
-	tree, err := g.checkoutTree(c.TreeId())
-	if err != nil {
-		return err
-	}
-	defer tree.Free()
-
-	reference := fmt.Sprintf("refs/heads/%s", branch)
-	log.Debugf("Setting reference '%s' on branch '%s'", reference, branch)
-	err = g.r.SetHead(reference)
-	if err != nil {
-		return err
-	}
-	_, err = g.r.References.Create(reference, c.Id(), true, branch)
-	return err
+	return c, nil
 }
 
 // branchIter execute the informed function against each branch in repository.
 func (g *GitRepo) branchIter(fn branchIterFn) error {
 	for _, branch := range g.sortBranches() {
-		if branch != "master" {
-			if err := g.checkoutBranch(branch); err != nil {
-				return err
-			}
+		c, err := g.LookupBranchCommit(branch)
+		if err != nil {
+			return fmt.Errorf("looking up '%s' commit: %w", branch, err)
+		}
+		defer c.Free()
+
+		tree, err := c.Tree()
+		if err != nil {
+			return fmt.Errorf("getting tree for commit: %w", err)
 		}
 
-		log.Infof("Transversing '%s' commits...", branch)
-		odb, err := g.r.Odb()
+		log.Infof("Traversing '%s' commits...", branch)
+		tree.Walk(func(s string, te *git.TreeEntry) int {
+			if te.Filemode != git.FilemodeCommit {
+				return 0
+			}
+			if err = fn(branch, c); err != nil {
+				return -1
+			}
+			return 0
+		})
 		if err != nil {
 			return err
 		}
-		defer odb.Free()
 
-		if err = fn(branch, odb); err != nil {
+		if err = fn(branch, c); err != nil {
 			return err
 		}
 	}
@@ -209,7 +167,11 @@ func (g *GitRepo) branchIter(fn branchIterFn) error {
 
 // ModifiedFiles for a given commit and tree, check what are the files that have changed, return them
 // as string slice.
-func (g *GitRepo) ModifiedFiles(c *git.Commit, tree *git.Tree) ([]string, error) {
+func (g *GitRepo) ModifiedFiles(c *git.Commit) ([]string, error) {
+	tree, err := c.Tree()
+	if err != nil {
+		return nil, fmt.Errorf("getting commit's tree: %w", err)
+	}
 	opts := &git.DiffOptions{}
 	modified := []string{}
 	parentCount := c.ParentCount()
@@ -253,60 +215,18 @@ func (g *GitRepo) ModifiedFiles(c *git.Commit, tree *git.Tree) ([]string, error)
 
 // CommitIter executed informed function on each branch commit.
 func (g *GitRepo) CommitIter(fn CommitIterFn) error {
-	return g.branchIter(func(branch string, odb *git.Odb) error {
-		head, err := g.r.Head()
-		if err != nil {
-			return err
-		}
-		defer head.Free()
-
-		c, err := g.r.LookupCommit(head.Target())
-		if err != nil {
-			return err
-		}
-		defer c.Free()
-
+	return g.branchIter(func(branch string, c *git.Commit) error {
 		tree, err := c.Tree()
 		if err != nil {
 			return err
 		}
 		defer tree.Free()
 
-		if err = fn(branch, c, tree, true); err != nil {
+		if err = fn(branch, c, true); err != nil {
 			return err
 		}
 
-		var counter = 1
-		return odb.ForEach(func(oid *git.Oid) error {
-			if g.cfg.CloneDepth > 0 && counter >= g.cfg.CloneDepth {
-				return nil
-			}
-
-			obj, err := g.r.Lookup(oid)
-			if err != nil {
-				return err
-			}
-			if obj.Type() != git.ObjectCommit {
-				return nil
-			}
-
-			c, err := obj.AsCommit()
-			if err != nil {
-				return err
-			}
-			defer c.Free()
-			if err = g.CheckoutCommit(branch, c); err != nil {
-				return err
-			}
-			tree, err := c.Tree()
-			if err != nil {
-				return err
-			}
-			defer tree.Free()
-
-			counter++
-			return fn(branch, c, tree, false)
-		})
+		return nil
 	})
 }
 
@@ -367,13 +287,10 @@ func (g *GitRepo) FetchBranch(branchName string) error {
 func NewGitRepo(cfg *Config, workdingDir string) (*GitRepo, error) {
 	log.Infof("Working directory at '%s'", workdingDir)
 	opts := &git.CloneOptions{
+		Bare: true,
 		FetchOptions: &git.FetchOptions{
 			DownloadTags: git.DownloadTagsAll,
 		},
-		CheckoutOpts: checkoutOpts,
-	}
-	if cfg.RelativeDir != "" {
-		opts.CheckoutOpts.Paths = []string{cfg.RelativeDir}
 	}
 
 	if cfg.ForceClone {
@@ -406,11 +323,6 @@ func NewGitRepo(cfg *Config, workdingDir string) (*GitRepo, error) {
 		return nil, fmt.Errorf("obtaining repository's head: %w", err)
 	}
 	defer head.Free()
-
-	err = repo.CheckoutHead(&git.CheckoutOptions{Strategy: git.CheckoutForce | git.CheckoutRecreateMissing})
-	if err != nil {
-		return nil, err
-	}
 
 	branches, err := extractBranches(repo)
 	if err != nil {

pkg/chartstreams/git_repo_test.go
@@ -26,8 +26,7 @@ func TestGitRepo_NewGitRepoUpstream(t *testing.T) {
 	require.NoError(t, err)
 
 	t.Run("CommitIter", func(t *testing.T) {
-		err := r.CommitIter(func(branch string, c *git.Commit, tree *git.Tree, head bool) error {
-			requirePopulatedGitDir(t, tempDir)
+		err := r.CommitIter(func(branch string, c *git.Commit, head bool) error {
 			return nil
 		})
 		assert.NoError(t, err)
@@ -58,8 +57,7 @@ func TestGitRepo_NewGitRepoLocal(t *testing.T) {
 	r, err := NewGitRepo(cfg, tempDir)
 	require.NoError(t, err)
 
-	err = r.CommitIter(func(branch string, c *git.Commit, tree *git.Tree, head bool) error {
-		requirePopulatedGitDir(t, tempDir)
+	err = r.CommitIter(func(branch string, c *git.Commit, head bool) error {
 		return nil
 	})
 	require.NoError(t, err)

commit 4146e1cdf141e52e76431df32e790f2ad5a29b22

Fixes regression harvesting charts
The changes introduced by 2c8c8cedeb92ad5d42df5a90222561ab0e0f2d33
don't take in consideration the `RelativeDir` configuration, resulting
in no charts being available in the index.

I've investigated the test case and, admittedly it isn't very robust
although increasing its robustness means to create a repository
with the same layout helm does, and have a stable set of charts
to run tests against and deserves its own PR.

pkg/chartstreams/chart_provider.go
@@ -31,7 +31,20 @@ var _ ChartProvider = &GitChartProvider{}
 
 // UpdateBranch fetches the latest changes from the remote.
 func (g *GitChartProvider) UpdateBranch(name string) error {
-	return g.gitRepo.FetchBranch(name)
+
+	if err := g.gitRepo.FetchBranch(name); err != nil {
+		return fmt.Errorf("fetching branch '%s': %w", name, err)
+	}
+	if err := g.BuildIndexFile(); err != nil {
+		return fmt.Errorf("building index file: %w", err)
+	}
+	return nil
+}
+
+func (g *GitChartProvider) BuildIndexFile() error {
+	var err error
+	g.indexFile, err = g.indexBuilder.Build()
+	return err
 }
 
 // GetIndexFile returns the helm repository index-file instance.
@@ -46,7 +59,7 @@ func (g *GitChartProvider) Initialize() error {
 		return err
 	}
 	g.indexBuilder = NewIndexBuilder(g.cfg, g.gitRepo)
-	if g.indexFile, err = g.indexBuilder.Build(); err != nil {
+	if err = g.BuildIndexFile(); err != nil {
 		return err
 	}
 	return nil

pkg/chartstreams/index_builder.go
@@ -53,6 +53,11 @@ func (i *IndexBuilder) listAllChartDirs(c *git.Commit) ([]string, error) {
 		if te.Filemode != git.FilemodeTree {
 			return 0
 		}
+		// since this method is specific to chart dirs, it is fine to skip the
+		// entry if it is in the root.
+		if path == "" {
+			return 0
+		}
 		parts := strings.Split(path, "/")
 		if len(parts) > 1 || len(parts) == 0 {
 			return 0
@@ -166,13 +171,22 @@ func (i *IndexBuilder) register(
 // inspectDirs loop chart directories, loading charts and registering them.
 func (i *IndexBuilder) inspectDirs(dirs []string, revision string, c *git.Commit, head bool) error {
 	for _, dir := range dirs {
-		files, err := i.g.GetFilesFromCommit(c, dir)
+		path := dir
+		if i.cfg.RelativeDir != "" {
+			path = i.cfg.RelativeDir + "/" + dir
+		}
+		path = strings.TrimPrefix(path, "//")
+		files, err := i.g.GetFilesFromCommit(c, path)
 		if err != nil {
 			return fmt.Errorf("getting files from commit '%s': %w", c.Id(), err)
 		}
+		if len(files) == 0 {
+			log.Infof("no files found in '%s' for commit '%s'", path, c.Id())
+			continue
+		}
 		chart, err := loader.LoadFiles(files)
 		if err != nil {
-			log.Warnf("error loading chart: '%s'", err)
+			log.Warnf("error loading chart with files from '%s': '%s'", path, err)
 			continue
 		}
 		if err = chart.Validate(); err != nil {